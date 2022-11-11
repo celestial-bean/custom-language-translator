e9b02d846bcea03448badbfd8c72b5101bfa4ede
Logan <logan326garcia@gmail.com>
2022-11-11 17:35:29 -0600

    modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
+
+//init stuff
 let languages = 1;
+let input = document.querySelector("#input");
+
+
+function translate(){
+  input.value
+}
+
 
 function newLanguage() {
+  //init things
   let div = document.createElement("div");
   let button = document.createElement("button");
+  let remove=document.createElement("button");
+
+//apply things
+  remove.textContent="-";
   button.classList.add("language");
   button.textContent = `language ${languages}`;
+  remove.addEventListener("click", function(){this.parentElement.remove(); languages--;})
+
+  //add language
   languages++;
+
+  //append things
   div.appendChild(button);
+  div.append(remove);
   document.querySelector("#languages").appendChild(div);
 
 }
 
 function main() {
-  let input = document.querySelector("#input");
 
 
 }
