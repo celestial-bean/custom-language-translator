1341b96ddf06e427ca896780cbe120420e59372e
Logan <92747680+celestial-bean@users.noreply.github.com>
2023-01-18 14:29:45 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,76 +1,87 @@
+
 //init stuff
-let languages=1;
-let input = document.getElementById("input");
-let output = document.getElementById("output");
-let data={};
-let seedBox=document.getElementById("languageSeed");
+function main() {
+  let languages = 1;
+  let input = document.getElementById("input");
+  let output = document.getElementById("output");
+  let data = {};
+  let seedBox = document.getElementById("languageSelector");
 
 
-function trans(){
-  output.value="";
- 
-  let In=data[seedBox.value].input;
-  let Out=data[seedBox.value].output;
-  if (!In.includes(" ")){
-  In+=" ";
-  }
-   if (!Out.includes(" ")){
-  Out+=" ";
-  }
- 
-  for (let i=0; i<input.value.length;i++){
-  let char = input.value[i];
-  output.value+=Out[In.indexOf(char)];
-  }
- 
-  console.log(In)
-  console.log(typeof In)
-}
+  function trans() {
+    output.value = "";
 
-
-function newLanguage() {
-
-
-  //init things
-  let div = document.createElement("div");
-  let button = document.createElement("button");
-  let remove=document.createElement("button");
-  let name=document.getElementById("name").value;
-  if (!name){
-    name="New language"+String(languages);
-  }
-  //check if name is already taken; add a number to it
-let count=0;
-  for (let i=0; i<data.length;i++){
-  if (data.name==name){
-    count+=1;
+    let In = data[seedBox.value].input;
+    let Out = data[seedBox.value].output;
+    if (!In.includes(" ")) {
+      In += " ";
     }
+    if (!Out.includes(" ")) {
+      Out += " ";
+    }
+
+    for (let i = 0; i < input.value.length; i++) {
+      let char = input.value[i];
+      output.value += Out[In.indexOf(char)];
+    }
+
   }
-  if (count){
-  name=name+count.toString();
+
+
+  function newLanguage() {
+
+
+    //init things
+    let div = document.createElement("div");
+    let button = document.createElement("button");
+    let remove = document.createElement("button");
+    let name = document.getElementById("name").value;
+    if (!name) {
+      name = "New language" + languages.toString();
+    }
+    //check if name is already taken; add a number to it
+    let count = 0;
+    for (let i = 0; i < data.length; i++) {
+      if (data.name == name) {
+        count += 1;
+      }
+    }
+    if (count) {
+      name = name + count.toString();
+    }
+
+
+
+    let charInput = document.getElementById("characterInput").value.split("");
+    let charOutput = document.getElementById("characterOutput").value.split("");
+
+
+    data[name] = {
+      name: name,
+      input: charInput,
+      output: charOutput
+    };
+
+    //apply things
+    remove.textContent = "-";
+    remove.style.float = "right";
+    remove.style.position = "relative";
+    button.classList.add("language");
+    button.textContent = name;
+    button.addEventListener("click", function() {
+      seedBox.value = data[name].name;
+    });
+    remove.addEventListener("click", function() {
+      this.parentElement.remove();
+    });
+
+    //append things
+    div.appendChild(button);
+    div.append(remove);
+    document.querySelector("#languages").appendChild(div);
+    languages += 1;
+
+
   }
 }
-
- 
-  let input=document.getElementById("characterInput").value.split("");
- let output=document.getElementById("characterOutput").value.split("");
- let seed=input+"separate"+output;
-
-  data[`${name}`]={name: `${name}`, input: `${input}`, output: `${output}`, seed:seed};
-
-//apply things
-  remove.textContent="-";
-  remove.style.float="right";
-  remove.style.position="relative";
-  button.classList.add("language");
-  button.textContent = `${name}`;
-  button.addEventListener("click", function(){seedBox.value=data[name].name;})
-  remove.addEventListener("click", function(){this.parentElement.remove();})
-
-  //append things
-  div.appendChild(button);
-  div.append(remove);
-  document.querySelector("#languages").appendChild(div);
-  languages+=1;
-
-}
+main()
